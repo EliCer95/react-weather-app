38e86d5dbd4c1765141f008458c81b86acdf2b36
EliCer95 <89043035+EliCer95@users.noreply.github.com>
2024-01-14 16:21:05 +0000

    Updated font-family, color and responsiveness

--- src/App.js
+++ src/App.js
@@ -7,26 +7,28 @@ export default function App() {
       <div className="container mt-5">
         <Weather defaultCity="Prague" />
       </div>
-      <footer className="footnote">
-        This project was coded by{" "}
-        <a
-          href="https://www.eliskacerna.com/"
-          target="_blank"
-          className="link"
-          rel="noreferrer"
-        >
-          Eliska Cerna
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/EliCer95/react-weather-app/tree/main"
-          target="_blank"
-          className="link"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="col">
+        <footer className="footnote">
+          This project was coded by{" "}
+          <a
+            href="https://www.eliskacerna.com/"
+            target="_blank"
+            className="link"
+            rel="noreferrer"
+          >
+            Eliska Cerna
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/EliCer95/react-weather-app/tree/main"
+            target="_blank"
+            className="link"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
